4d6361a7f002ff7a7dd00e919db4efb68a29cdb0
Lumbani Chilufya <dynamite112lc@gmail.com>
2024-07-04 14:44:22 +0200

    Functions to play round implemented. Results of each round displayed on console.
    Function to play game implemented.
    
    Displays final score of game.

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-console.log(Math.floor(Math.random() * 3));
+// Code to play simple Rock, Paper, Scissors game in console.
 
 function getHumanChoice() {
   human = prompt("Enter Rock, Paper or Scissors: ");
@@ -19,4 +19,50 @@ function getComputerChoice() {
   }
 }
 
-getHumanChoice();
+function playGame() {
+  humanScore = computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+      console.log("You win! Rock beats Scissors!");
+      humanScore++;
+    }
+    if (humanChoice == "rock" && computerChoice == "paper") {
+      console.log("You lose! Paper beats Rock!");
+      computerScore++;
+    }
+    if (humanChoice == "paper" && computerChoice == "scissors") {
+      console.log("You lose! Scissors beats Paper!");
+      computerScore++;
+    }
+    if (humanChoice == "paper" && computerChoice == "rock") {
+      console.log("You win! Paper beats Rock!");
+      humanScore++;
+    }
+    if (humanChoice == "scissors" && computerChoice == "rock") {
+      console.log("You lose! Rock beats Scissors!");
+      computerScore++;
+    }
+    if (humanChoice == "scissors" && computerChoice == "paper") {
+      console.log("You win! Scissors beats Paper!");
+      humanScore++;
+    }
+    if (humanChoice == computerChoice) {
+      console.log("It's a draw!");
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+  }
+
+  console.log("Computer: " + computerScore + " Human: " + humanScore);
+}
+
+playGame();
